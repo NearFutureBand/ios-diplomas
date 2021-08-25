bd23d9b896f014e3ba2085576729e48351c5918a
Pavel Belyakov <theflashofsonic@gmail.com>
2021-08-25 14:58:59 +0300

    added ImageStore proect

---
+++ ImageStore/ImageStore/PictureViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class PictureViewController: UIViewController {
+  
+  weak var picture: Picture?
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      
+      print(picture)
+    }
+
+
+}
